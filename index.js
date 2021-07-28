38e36f9a596f81694402654ea0b90447bb71c426
krishnakant yadav <krishnakantyadav19@gmail.com>
2021-07-29 00:47:15 +0530

    master branch

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = 8000
 const expressLayouts = require('express-ejs-layouts')
+const db = require('./config/mongoose')
+
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
+app.use(cookieParser())
 
 app.use(express.static('./assets'))
 
